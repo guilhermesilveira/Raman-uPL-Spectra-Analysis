4108d4f8178176d3aca62fa1c391ac658325d578
Guilherme Silveira <guilherme.silveira@caelum.com.br>
2021-01-08 15:42:22 -0300

    fixing the last element issue
    The last element from dati would have one last element missing. That is because in this case i is actually one less than in all other cases (because we are predicting, ahead, instead of at the moment of the change of x,y)
    
    I also migrated print and next to python 3
    
    I also used numpy features instead of manual copying of arrays

--- src/Raman/mapping_xy/Fit.py
+++ src/Raman/mapping_xy/Fit.py
@@ -133,9 +133,9 @@ def FitLorrentz2(x, y, xmin=555, xmax=572, DEBUGFILE=False):
 	return [xfit[0],xfit[1],fit(xm)]	
 
 def ParseFile(f):
-	with open(f, 'rb') as csvfile:
+	with open(f, 'r') as csvfile:
 		rd = csv.reader(csvfile, delimiter='	')
-		row 		= rd.next()
+		row 		= next(rd)
 		x			= []
 		y			= []
 		eV  		= []
@@ -149,53 +149,28 @@ def ParseFile(f):
 		y 			= np.array(y)
 		eV 			= np.array(eV)
 		dati 		= np.array(dati)
+		
+		last_i = 0
+		j	= 0
 
-		
-		x0 	= x[0]
-		y0 	= y[0]
-		xi0 = x[0]
-		yi0 = y[0]
-		i0	= 0
-		j0	= 0
-		j	= j0
-		m	= 1
-		
-		for h in range (dati.shape[0]): #compter le nombre de configuration (x,y)
-			if x[h]!=xi0 or y[h]!=yi0:
-				m=m+1
-				xi0=x[h]
-				yi0=y[h]
+		all_data = np.column_stack((x,y))
+		m = len({(x,y) for x,y in all_data})
 		
 		for i in range (dati.shape[0]):
 			
-			if x[i]!=x0 or y[i]!=y0 or i == dati.shape[0]-1:
+			is_last_reading = i == dati.shape[0]-1
+			if x[i]!=x[last_i] or y[i]!=y[last_i] or is_last_reading:
 				
-				if i0 == 0:	#on selectionne le raman shift mais la premiere fois est suffisante
-					dat = np.zeros((i+2,m)) #dat est de dimension 2, contrairement a dati
-					eVj	= np.zeros(i+2)
-					eVj[0] = 0
-					for n in range (eVj.shape[0]):	#on restreint en decalant de 2 pour x et y
-						eVj[n]= eV[n]
-				
-				
-				datj	= np.zeros(i-i0+2)#+2 pour le x et y
-				for b in range (i0,i):
-					datj[b-i0] = dati[b]
-				
-				for k in range(datj.shape[0],2): #on decale tout de 2 pour pouvoir noter la valeur de x et y
-					datj[k] = datj[k-2]
-				datj[0]	= x0
-				datj[1]	= y0
-				
-				for a in range (datj.shape[0]): #on rempli dat
-					dat[a,j]= datj[a]				
-		
-				x0=x[i] #on decale les valeurs pour la prochaine colonne
-				y0=y[i]
-				i0=i
-				j=j+1
-			
-			i=i+1
+				if last_i == 0:	#on selectionne le raman shift mais la premiere fois est suffisante
+					dat = np.zeros((i+2, m)) #dat est de dimension 2, contrairement a dati
+					eVj = eV[:i+2]
+
+				dat[:-2, j] = dati[last_i:i + is_last_reading]
+				dat[0,j]	= x[last_i]
+				dat[1,j]	= y[last_i]
+
+				last_i = i
+				j += 1
 			
 	
 		return [m, eVj, dat]
@@ -233,9 +208,10 @@ if __name__=="__main__":
 		yi = dat[1,i]
 		[A1i,x10i,gam1i] 	= FitLorrentz1(eV, dat[:,i] , DEBUGFILE='mes1_%d.png'%(i))
 		[A2i,x20i,gam2i] 	= FitLorrentz2(eV, dat[:,i] , DEBUGFILE='mes2_%d.png'%(i))
-		print "Fitting data: x=%0.1f, y=%0.1f, %0.3e,%0.3e,%0.3e,%0.3e" % (xi, yi, A1i, x10i, A2i, x20i)
+		print("Fitting data: x=%0.1f, y=%0.1f, %0.3e,%0.3e,%0.3e,%0.3e" % (xi, yi, A1i, x10i, A2i, x20i))
 		myfile = open ("fitresultB1_12_xy.csv","a")
 		myfile.write ("\n%0.1f,%0.1f,%0.3e,%0.3f,%0.3e,%0.3f" % (xi, yi, A1i, x10i, A2i, x20i))
 		myfile.close()
 	
+
 	
